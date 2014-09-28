a157f5c5cde2914d95b2a05643f7893dfc305f70
Milan <Milan@Milan-PC>
2014-09-28 23:19:37 +0200

    Signal

--- Test_proj/test1.c
+++ Test_proj/test1.c
@@ -10,6 +10,7 @@
 int main(void) {
 	printf("Ahoj Milan kradnem ti program\n");
 	printf("Marek, pojdme si spolu zazpivat.\n");
-	printf("Zuzanka sa prida a Marek a Milan sa musia tiez: A teraz vsedcia spolu Ja sem GEY sem GEY sem tepleeeej, ja sem GEY sem GEY sem uuuuplne teplej");
+	printf("Zuzanka sa prida a Marek a Milan sa musia tiez: A teraz vsedcia spolu Ja sem GEY sem GEY sem tepleeeej, ja sem GEY sem GEY sem uuuuplne teplej\n");
+	printf("Ja tu jenom: Cekam na signal, ten z nebe neprichazi, a tak cekam daaal.\n");
 	return 0;
 }
