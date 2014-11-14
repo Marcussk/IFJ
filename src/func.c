9c81832b62a861f8b4d4dc067b412881a3e05435
nic30 <nic30g@gmail.com>
2014-11-15 00:25:00 +0100

    malloc fix

--- src/func.c
+++ src/func.c
@@ -2,7 +2,6 @@
 
 // to make this use: gcc func.c errorHandler.c ifjTypes.h ifjTypes.c
 
-
 int func_len(char *str) {
 
 	return strlen(str);
@@ -15,13 +14,14 @@ int func_len(char *str) {
 char * func_copy(char *f_str, int i, int n) {
 	int strIndex;
 	int offset = i - 1;
-	if(i < 1)
+	if (i < 1)
 		rt_error("Builtin function copy does not support i <1");
 	if (strlen(f_str) < (offset + n)) {
-		rt_error("Builtin function copy cano't be performed because original string is too short\n");
+		rt_error(
+				"Builtin function copy cano't be performed because original string is too short\n");
 	}
-	char * newStr = malloc(i+1, sizeof(char));
-	for(strIndex = 0; strIndex < n; strIndex++){
+	char * newStr = malloc(i + 1 * sizeof(char));
+	for (strIndex = 0; strIndex < n; strIndex++) {
 		newStr[strIndex] = f_str[strIndex + offset];
 	}
 	newStr[strIndex] = '\0';
