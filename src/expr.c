2bb1019d7d641ec82663aa4bab9aa3ad5adafd6d
Milan <Milan@Milan-PC>
2014-11-22 18:07:45 +0100

    Added files with expression analysis - contains very basic functions. Precedence table moved to expr.c

---
+++ src/expr.c
@@ -0,0 +1,137 @@
+#include "expr.h"
+
+HashTable *SymbolTable;
+Stack *stack;
+ExprToken *ExprEndToken;
+ExprToken *ExprLastToken;
+ExprToken *TopMostTerminal;
+
+typedef enum {
+	shift, reduce, error, equal
+} precedenceRule;
+
+//keep synergyc with tokenMap.h/Token
+precedenceRule prTable[16][16] =
+ //+       -       *       /       <       >       <=      >=      =       <>      (      )       ,       $       var    fn
+{{ reduce, reduce, shift,  shift,  reduce, reduce, reduce, reduce, reduce, reduce, shift, reduce, reduce, reduce, shift, shift   },   // +
+ { reduce, reduce, shift,  shift,  reduce, reduce, reduce, reduce, reduce, reduce, shift, reduce, reduce, reduce, shift, shift   },   // -
+ { reduce, reduce, reduce, reduce, reduce, reduce, reduce, reduce, reduce, reduce, shift, reduce, reduce, reduce, shift, shift   },   // *
+ { reduce, reduce, reduce, reduce, reduce, reduce, reduce, reduce, reduce, reduce, shift, reduce, reduce, reduce, shift, shift   },   // /
+ { shift,  shift,  shift,  shift,  reduce, reduce, reduce, reduce, reduce, reduce, shift, reduce, reduce, reduce, shift, shift   },   // <
+ { shift,  shift,  shift,  shift,  reduce, reduce, reduce, reduce, reduce, reduce, shift, reduce, reduce, reduce, shift, shift   },   // >
+ { shift,  shift,  shift,  shift,  reduce, reduce, reduce, reduce, reduce, reduce, shift, reduce, reduce, reduce, shift, shift   },   // <
+ { shift,  shift,  shift,  shift,  reduce, reduce, reduce, reduce, reduce, reduce, shift, reduce, reduce, reduce, shift, shift   },   // >
+ { shift,  shift,  shift,  shift,  reduce, reduce, reduce, reduce, reduce, reduce, shift, reduce, reduce, reduce, shift, shift   },   // =
+ { shift,  shift,  shift,  shift,  reduce, reduce, reduce, reduce, reduce, reduce, shift, reduce, reduce, reduce, shift, shift   },   // <
+ { shift,  shift,  shift,  shift,  shift,  shift,  shift,  shift,  shift,  shift,  shift, equal,  equal,  error,  shift, shift   },   // (
+ { reduce, reduce, reduce, reduce, reduce, reduce, reduce, reduce, reduce, reduce, equal, reduce, reduce, reduce, error, reduce  },   // )
+ { shift,  shift,  shift,  shift,  shift,  shift,  shift,  shift,  shift,  shift,  shift, equal,  equal,  error,  shift, shift   },   // ,
+ { shift,  shift,  shift,  shift,  shift,  shift,  shift,  shift,  shift,  shift,  shift, error,  error,  error,  shift, shift   },   // $
+ { reduce, reduce, reduce, reduce, reduce, reduce, reduce, reduce, reduce, reduce, error, reduce, reduce, reduce, error, error   },   //var
+ { error,  error,  error,  error,  error,  error,  error,  error,  error,  error,  equal, error,  error,  error,  error, error   }    //fn
+};
+
+char terminals[] = {['+']=0, ['-']=1, ['*']=2, ['/']=3,
+					['<']=4, ['>']=5, ['L']=6, ['G']=7,
+					['=']=8, ['!']=9, ['(']=10, [')']=11,
+					[',']=12, ['$']=13, ['i']=14, ['f']=15};
+
+char tokenToChar(Token token)
+{
+	switch(token)
+	{
+	case t_notEqv:
+		return '!';
+	case t_lessOrEqv:
+		return 'L';
+	case t_greaterOrEqv:
+		return 'G';
+ 	case t_func:
+ 		return 'f';
+	case t_id:
+	case t_num_int:
+	case t_num_real:
+	case t_str_val:
+		return 'i';
+	default:
+		return token;
+	}
+}
+
+int getTokenType(Token token)
+{
+	switch (token)
+	{
+		case t_true:
+		case t_false:
+			return boolean;
+		case t_num_int:
+		case t_num_real:
+		case t_str_val:
+			return token;
+		default:
+			return none;
+	}
+}
+
+void ExprTokenInit(ExprToken *token)
+{
+	token->content = '$';
+	token->type = terminal;
+	token->datatype = none;
+}
+
+void tokenToExpr(ExprToken *Expr, Token *token)
+{
+	Expr->content = tokenToChar(*token);
+	Expr->type = terminal;
+	Expr->datatype = getTokenType(*token);
+}
+
+ExprToken *findTopMostTerminal(Stack *s)
+{
+	StackNodeT itr = s->top;
+	while (itr != NULL){
+		if (itr->data->type == terminal)
+			return itr->data;
+		itr = itr->next;
+	}
+}
+
+void ExprInit(Stack *TMPstack, HashTable *TMPSymbolTable)
+{
+	SymbolTable = TMPSymbolTable;
+	stack = TMPstack;
+	ExprTokenInit(EndToken);
+	ExprTokenInit(LastToken);
+	Stack_push(stack, EndToken);
+}
+
+void expression(SyntaxAnalyzer *self, Stack *TMPstack, HashTable *TMPSymbolTable)
+{
+	ExprInit(TMPstack, TMPSymbolTable);
+	self->lastToken = LexParser_gen(self->lp);
+
+	do{
+		tokenToExpr(ExprLastToken, self->LastToken); // "copy" content of LastToken to ExprLastToken
+		topMostTerminal = findTopMostTerminal(stack)
+
+		switch(prTable[topMostTerminal][ExprLastToken])
+		{
+			case shift:
+				// Vloz zacatek handle
+			case equal:
+				// push ExprLastToken
+				break;
+			case reduce:
+				// Prohledavej zasobnik, dokud nenarazis na handle, najdi pravidlo
+				// a zredukuj
+				break;
+			case error:
+				// Zahlas syntaktickou chybu
+		}
+
+	self->lastToken = LexParser_gen(self->lp));
+
+	} while (stack->top != EndToken->content || (self->LastToken != t_end || self->LastToken != t_scolon)
+}
---
+++ src/expr.h
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "errorHandler.h"
+#include "lexParser.h"
+#include "strRoutines.h"
+#include "syntaxAnalyzer.h"
+#include "tokenMap.h"
+
+// TODO: copy function bodies for stack
+
+typedef enum {
+	terminal, nonterminal
+} ExprType;
+
+typedef enum {
+	none, boolean = 15, integer, real, string // none is for '$' and initialized ExprTokens
+} ExprDataType;
+
+typedef struct exprtoken{
+	char content, // contains terminal converted to a single char - terminal or E
+	ExprType type,
+	ExprDataType datatype
+} ExprToken;
+
+
+typedef ExprToken stackElementT;
+
+typedef struct stackTag {
+	stackElementT data;
+	struct stackTag *next;
+} stackNodeT;
+
+typedef struct {
+	stackNodeT *top;
+} Stack;
+
+Stack * Stack__init__();
+void Stack_push(Stack * s, stackElementT data) ;
+stackElementT Stack_pop(Stack * s);
+
