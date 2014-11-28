b7fbdb01c7fd7641673e5760aca16af052436038
Marcussk <keram.beno@gmail.com>
2014-11-28 03:38:48 +0100

    Test sample

---
+++ samples/sample9.pas
@@ -0,0 +1,14 @@
+{ Jednoduchy test na if }
+var
+    a : integer;
+begin
+    readln(a);
+    if a < 0 then
+    begin
+        write('ano')
+    end
+    else
+    begin
+        write('nie')
+    end
+end.
