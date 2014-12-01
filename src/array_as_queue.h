c56048c3e9ed83267d52faaa087cb7e7511a0270
zuzana <zuzanast21@gmail.com>
2014-12-01 16:07:05 +0100

    queue

---
+++ src/array_as_queue.h
@@ -0,0 +1,23 @@
+#include "stdlib.h"
+#include "stdio.h"
+
+#include "ifj_types.h"
+#include "error_handler.h"
+#include "instruction.h"
+
+
+#define QUEUE_START_SIZE 128
+
+typedef struct {
+	Instruction *QueueArr; //Instruction QueueArr[prealocated];
+	int first;
+	int last;
+	int actual;
+	int prealocated;
+} InstrQueueArr;
+
+void InstrQueueArr__init__(InstrQueueArr *self);
+Instruction* InstrQueueArr_next(InstrQueueArr* self);
+void InstrQueueArr_insert(InstrQueueArr *self, Instruction i);
+Instruction* InstrQueueArr_atIndex(InstrQueueArr* self, int index);
+void InstrQueueArr__dell__(InstrQueueArr *self);
