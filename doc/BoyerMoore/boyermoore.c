76862d287a4978e2fd38d06b42d703496d4d0805
Marcussk <keram.beno@gmail.com>
2014-12-02 18:21:32 +0100

    Reworked, polished everything should now work

--- doc/BoyerMoore/boyermoore.c
+++ doc/BoyerMoore/boyermoore.c
@@ -5,7 +5,7 @@
 #define MAX_SIZE 255
 #define SUFFSIZE 255
 
-//#define DEGUG
+#define DEGUG
 
 #ifdef DEBUG
 #define DEBUGING(body) body
@@ -27,26 +27,28 @@ void badcharfill( char *str, int length, int badchar[MAX_SIZE] );
 
 int main() 
 {
-	test(1,"ABAAABCD","ABC",5);
-	test(2,"BCDEFGABCDEF","ABCD",7);
+    test(1,"ABAAABCD","ABC",5);
+    test(2,"BCDEFGABCDEF","ABCD",7);
     test(3,"ABDXXXXXABCD","CD",11);
     test(4,"ABDXXXXXABCD","DE",0);
     test(5,"ABAAABCD","ABCD",5);
+    test(6,"ADFSFBCD","",1);
+    test(7,"","ABCD",0);
     return 0;
 
 }
 
 void badcharfill( char *str, int length, int badchar[] ) {
-	int i;
-	//initialize array
-	for (i = 0; i < MAX_SIZE; i++) {
-		badchar[i] = length; 
-	}
-	//fill array of patlength with last occurence of character
-	//array is rewritten during iterations and array[currentchar] = positioninpatter
-	for (i = 0; i < length; i++) {
-		badchar[(int) str[i]] = length -i -1;
-	}
+    int i;
+    //initialize array
+    for (i = 0; i < MAX_SIZE; i++) {
+        badchar[i] = length; 
+    }
+    //fill array of patlength with last occurence of character
+    //array is rewritten during iterations and array[currentchar] = positioninpatter
+    for (i = 0; i < length; i++) {
+        badchar[(int) str[i]] = length -i -1;
+    }
 }
 
 void suffixes(char *str, int length, int *suff) {
@@ -55,67 +57,67 @@ void suffixes(char *str, int length, int *suff) {
    suff[length - 1] = length;
    g = length - 1;
    for (i = length - 2; i >= 0; --i) {
-      if (i > g && suff[i + length - 1 - f] < i - g)
-         suff[i] = suff[i + length - 1 - f];
-      else {
-         if (i < g)
-            g = i;
-         f = i;
-         while (g >= 0 && str[g] == str[g + length - 1 - f])
-            --g;
-         suff[i] = f - g;
-      }
-   }
+      	if (i > g && suff[i + length - 1 - f] < i - g)
+        	suff[i] = suff[i + length - 1 - f];
+      	else {
+        	if (i < g)
+            	g = i;
+         	f = i;
+         	while (g >= 0 && str[g] == str[g + length - 1 - f])
+            	--g;
+         	suff[i] = f - g;
+      	}
+   	}
 }
 
 void processsuff(char *str, int length, int goodsuff[]) {
     int i, j, suff[SUFFSIZE];
- 
-   suffixes(str, length, suff);
- 
-   for (i = 0; i < length; ++i)
-      goodsuff[i] = length;
-   j = 0;
-   for (i = length - 1; i >= 0; --i)
-      if (suff[i] == i + 1)
-         for (; j < length - 1 - i; ++j)
-            if (goodsuff[j] == length)
-               goodsuff[j] = length - 1 - i;
-   for (i = 0; i <= length - 2; ++i)
-      goodsuff[length - 1 - suff[i]] = length - 1 - i;
+   	suffixes(str, length, suff);
+    for (i = 0; i < length; ++i)
+    	goodsuff[i] = length;
+    j = 0;
+    for (i = length - 1; i >= 0; --i)
+      	if (suff[i] == i + 1)
+         	for (; j < length - 1 - i; ++j)
+            	if (goodsuff[j] == length)
+               		goodsuff[j] = length - 1 - i;
+   	for (i = 0; i <= length - 2; ++i)
+      	goodsuff[length - 1 - suff[i]] = length - 1 - i;
 }
 
 int find(char *txt, char *pat) {
-	int patlength = strlen(pat);
-	int txtlength = strlen(txt);
-	int shift = 0;
-	int matchindex;
-	int badchar[MAX_SIZE];
+    int patlength = strlen(pat);
+    int txtlength = strlen(txt);
+    int shift = 0;  
+    int matchindex;
+    int badchar[MAX_SIZE];
     int goodsuff[SUFFSIZE];
 
-	badcharfill(pat, patlength, badchar);
+    if (strcmp(pat, "") == 0)
+    {
+        return 1;
+    }
+
+    badcharfill(pat, patlength, badchar);
     processsuff(pat, patlength, goodsuff);
 
-	while (shift <= txtlength - patlength) {
-		matchindex = patlength - 1;
-		//Check chars from right to left to see if we have pattern match
-		for (matchindex = patlength - 1; matchindex >= 0 && pat[matchindex] == txt[matchindex +shift]; --matchindex);
+    while (shift <= txtlength - patlength) { 
+        matchindex = patlength - 1;
+        //Check chars from right to left to see if we have pattern match
+        for (matchindex = patlength - 1; matchindex >= 0 && pat[matchindex] == txt[matchindex +shift]; --matchindex);
         DEBUGING(printf("Match index: %d\n",matchindex);)
-		//Succesfully found match (patlength of chars are matching)
-			if (matchindex < 0) {
-				//printf("Finding: %d \n", shift + 1);
-				return shift + 1;
-				//if we can move shift so it aligns with text
-				shift += goodsuff[0];
+        //Succesfully found match (patlength of chars are matching)
+            if (matchindex < 0) {
+                //printf("Finding: %d \n", shift + 1);
+                return shift + 1;
+                //if we can move shift so it aligns with text
+                shift += goodsuff[0];
             }
-			else {
+            else {
                 shift += max(goodsuff[matchindex], badchar[(int)txt[matchindex + shift]] - patlength +1 + matchindex);
             }
-	}		
-		
-		
-	
-	return 0;
+    }       
+    return 0;
 }
 
 int max(int a, int b) {
