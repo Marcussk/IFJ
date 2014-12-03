0e0d703ccab875a4d7642b56e77663cae4d51956
Marcussk <keram.beno@gmail.com>
2014-12-03 01:27:06 +0100

    Prepared converts

--- src/semantic_analyzer.c
+++ src/semantic_analyzer.c
@@ -16,3 +16,16 @@ void SemAnalyzer_checkcond(tIFJ param) {
 	}
 	//printf("Condition: %s\n",iVar_type2str(param) );
 }
+
+void SemAnalyzer_typeconvert (tIFJ param1, tIFJ param2) {
+	//int real
+	if (( param1 == 17) && (param2 == 18)) {
+		//convert param1
+	}
+	
+	//real int
+	if (( param1 == 18) && (param2 == 17)) {
+		//convert param2
+	}
+
+}
--- src/semantic_analyzer.h
+++ src/semantic_analyzer.h
@@ -9,4 +9,5 @@
 //the entire header file file
 void SemAnalyzer_checktypes(tIFJ param1, tIFJ param2);
 void SemAnalyzer_checkcond(tIFJ param);
+void SemAnalyzer_typeconvert (tIFJ param1, tIFJ param2);
 #endif
