1fc0a22d872f1c378484e85ac636f90897d71924
Milan <Milan@Milan-PC>
2014-12-13 12:49:17 +0100

    Simple tests for functions with no parameter

---
+++ samples/sample293.pas
@@ -0,0 +1,18 @@
+{ Empty function }
+var
+	comp : boolean;
+{ Definice funkce }
+
+function foo() : integer;
+var 
+	a : integer;
+begin
+	foo := 5
+end;
+
+begin
+
+comp := (2+2)*foo(() = foo() * (2+2);
+write(comp)
+
+end.
