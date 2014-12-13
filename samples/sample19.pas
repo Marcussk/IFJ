99209fac6c623130aaa9ddd21a22ab8d9829e05a
Marcussk <keram.beno@gmail.com>
2014-12-13 05:49:58 +0100

    That last merge was a fail (test still not workin)

--- samples/sample19.pas
+++ samples/sample19.pas
@@ -1,18 +1,5 @@
 { nedochadza k pretypovaniu string real}
 var
-<<<<<<< HEAD
-	a : integer;
-begin 
-	a := 5 ;
-	repeat
-	begin
-		write(a);
-		a := a - 1
-	end
-	until a > 0 ;
-	write('vysl:', a, ''#10'')
-end.	
-=======
 	vysl : string;
 	assign : real;
 { Hlavni telo programu }
@@ -21,4 +8,3 @@ begin
 	vysl := assign;
     write(vysl)
 end.
->>>>>>> 7934910f377e6d3f40796e152699f55b570a1cea
